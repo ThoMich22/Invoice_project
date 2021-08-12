f9f88a9a27b6ccac5cbbbe1e6aed49e4ee6dbfe0
ThoMich22 <thomas_8_69@hotmail.com>
2021-08-12 10:52:10 +0200

    add controller product and views and routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,49 @@
 class ProductsController < ApplicationController
+  def index
+    @product = Product.all 
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @products = Product.new
+  end
+
+  def create 
+    @product = Product.create(product_params)
+    if @product.save
+      flash[:success] = "Création du nouveau client réussie "
+      redirect_to @product
+    else
+      flash[:error] = "Echec dans la création du nouveau client"
+      redirect_to @product      
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:success] = "Mise à jour client réussie "
+      redirect_to @product
+    else
+      flash[:error] = "Echec dans la mise à jours du client"
+      render :edit    
+    end
+  end
+
+  def destroy
+    Product.find(params[:id]).destroy 
+  end
+
+  private 
+
+    def product_params
+      params.require(:product).permit(:title, :description, :price)
+    end
 end
