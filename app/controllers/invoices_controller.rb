7cee58bb57ebfcb490bc12fb99753dd78a88f94f
ThoMich22 <thomas_8_69@hotmail.com>
2021-08-13 18:52:07 +0200

    add invoice controller + views

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,2 +1,58 @@
 class InvoicesController < ApplicationController
+  def index
+    @invoice = Invoice.all 
+  end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+    @clients = Client.all
+  end
+
+  def new
+    @invoice = Invoice.new
+  end
+
+  def create 
+    @invoice = Invoice.create(invoice_params)
+    if @invoice.save
+      flash[:success] = "Création du nouveau client réussie "
+      redirect_to invoices_path
+    else
+      flash[:error] = "Echec dans la création du nouveau client"
+      redirect_to @invoice     
+    end
+  end
+
+  def edit
+    @invoice = Invoice.find(params[:id])
+  end
+
+  def update
+    @invoice = Invoice.find(params[:id])
+    if @invoice.update(invoice_params)
+      flash[:success] = "Mise à jour client réussie "
+      redirect_to invoices_path
+    else
+      flash[:error] = "Echec dans la mise à jours du client"
+      render :edit    
+    end
+  end
+
+  def destroy
+    @invoice= Invoice.find(params[:id])
+    if @invoice.destroy
+      flash[:success] = "Produit supprimé"
+      redirect_to invoices_path
+    else
+      flash[:error] = "Echec dans la mise à jours du client"
+      render :edit    
+    end
+  end
+
+  private 
+
+    def invoice_params
+      params.require(:invoice).permit(:date, :statue, :client_id)
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 
   resources :clients
   resources :products
+  resources :invoices
 
 end
